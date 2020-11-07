b7b49bad141aca90bfa0d8ff30aaa5b82d3709a1
seanianking <seanianking@gmail.com>
2020-11-06 22:22:00 -0700

    testing side nav for search page

--- haystack/src/components/SearchPanel.js
+++ haystack/src/components/SearchPanel.js
@@ -2,41 +2,14 @@ import React from 'react'
 
 const SearchPanel = () => {
     return (
-        <aside class="menu">
-            <p class="menu-label">Coding Language</p>
+        <aside className="menu">
 
-            <select name='language'>
-                <option>Any Language</option>
-                <option>C#</option>
-                <option>Java</option>
-                <option>JavaScript</option>
-                <option>PHP</option>
-                <option>Python</option>            
-            </select>
+            <p className="menu-label">Coding Language</p>
 
-            <p class="menu-label">Tags</p>
-            <ul class="menu-list">
-                <li><a>Team Settings</a></li>
-                <li>
-                    <a class="is-active">Manage Your Team</a>
-                    <ul>
-                        <li><a>Members</a></li>
-                        <li><a>Plugins</a></li>
-                        <li><a>Add a member</a></li>
-                    </ul>
-                </li>
-                <li><a>Invitations</a></li>
-                <li><a>Cloud Storage Environment Settings</a></li>
-                <li><a>Authentication</a></li>
-            </ul>
-            <p class="menu-label">
-                Transactions
-  </p>
-            <ul class="menu-list">
-                <li><a>Payments</a></li>
-                <li><a>Transfers</a></li>
-                <li><a>Balance</a></li>
-            </ul>
+            <p className="menu-label">Categories</p>
+           
+            <p className="menu-label">Transactions</p>
+            
         </aside>
     )
 }
