51be703f1944889349d70749aa937b12dbadebd9
unknown <d_bille@yahoo.com>
2020-11-10 17:03:06 -0600

    created server folder and server.js file

---
+++ haystack/server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.static('public'))
+
+app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.get('/test', (req, res) => {
+  res.json({message: 'we out here'})
+})
+
+app.listen(3001, () => {
+  console.log('Listening on port 3000')
+})
