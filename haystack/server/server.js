0225e85cf9261dff32f89100b4c440ec9689d117
unknown <d_bille@yahoo.com>
2020-11-10 19:52:44 -0600

    okaaayyyyy

--- haystack/server/server.js
+++ haystack/server/server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const PORT = 3001
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(cors())
+app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+  );
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
-})
+});
