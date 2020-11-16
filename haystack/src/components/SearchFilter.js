18835aebe41fbf074f9cfaf5cb75d32075882aa2
seanianking <seanianking@gmail.com>
2020-11-16 00:53:05 -0700

    added file and reference code for the filter panel

---
+++ haystack/src/components/SearchFilter.js
@@ -0,0 +1,13 @@
+// Will be an aside menu to filter results using
+// Weekly Download, most recent update, stars, etc.
+import React from 'react';
+
+const SearchFilter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchFilter;
--- haystack/src/pages/SearchResults.js
+++ haystack/src/pages/SearchResults.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-
+// import SearchFilter from '../components/SearchFilter';
 
 const SearchResults = () => {
     return (
         <div>
             <h1>Results</h1>
-            
+        {/* <SearchFilter/> */}
         <SearchBar/>
         </div>
     )
