fc0d99a740e48d91e80fc6e00da1909b77919065
Silvester Jumba <onsilverstar@gmail.com>
2020-12-17 17:26:01 -0500

    added search features to component

--- haystack/src/components/SearchBar.js
+++ haystack/src/components/SearchBar.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const SearchBar = () => {
     const [DropdownState, setDropdownState] = useState(Boolean);
+    const [array, setState]=useState({language:"javascript",keyword:""})
+    const [searchResult, setSearchResultState]=useState({result:[]})
 
     return (
         <div className='box mx-3'>
@@ -9,13 +11,13 @@ const SearchBar = () => {
                 <label className='label my-1'>Choose a language:</label>
                 <div className='control my-1'>
                     <label className='radio pr-1'>
-                        <input name='language' type='radio' defaultValue='JavaScript' onChange={() => setDropdownState(false)} /> JavaScript</label>
+                        <input name='language' type='radio' defaultValue='JavaScript' onChange={(event)=>{setDropdownState(false);setState({language:event.target.value})}}/> JavaScript</label>
                     <label className='radio pr-1'>
-                        <input name='language' type='radio' defaultValue='Python' onChange={() => setDropdownState(false)} /> Python</label>
+                        <input name='language' type='radio' defaultValue='Python' onChange={(event)=>{setDropdownState(false);setState({language:event.target.value})}}/> Python</label>
                     <label className='radio pr-1'>
-                        <input name='language' type='radio' defaultValue='Ruby' onChange={() => setDropdownState(false)} /> Ruby</label>
+                        <input name='language' type='radio' defaultValue='Ruby' onChange={(event)=>{setDropdownState(false);setState({language:event.target.value})}} /> Ruby</label>
                     <label className='radio pr-1'>
-                        <input name='language' type='radio' defaultValue='null' onChange={() => setDropdownState(!DropdownState)} /> Other Language</label>
+                        <input name='language' type='radio' defaultValue='null' onChange={(event)=>{setDropdownState(false);setState({language:event.target.value})}} /> Other Language</label>
                 </div>
                 <div className='control is-expanded'>
                     <div className='select my-1 is-fullwidth' style={{ display: DropdownState === false ? 'none' : 'block' }}>
@@ -31,7 +33,12 @@ const SearchBar = () => {
             </div>
             <div className='field'>
                 <div className='control is-medium'>
-                    <input className='input is-medium' type='text' placeholder='Search here' />
+                    <input className='input is-medium' type='text' placeholder='Search here' 
+                    onChange={(event)=>setState({keyword:event.target.value})}/>
+                    <input type="submit" value="Search" onClick={(event) => {
+event.preventDefault()
+fetch("https://api.github.com/search/repositories?q="+array["language"]+"language:"+array["keyword"]+"&sort=stars&order=desc").then
+(res=>{return res.json()}).then(data=>{setSearchResultState({result:data.items});console.log(searchResult)})}}/>
                 </div>
             </div>
         </div>
